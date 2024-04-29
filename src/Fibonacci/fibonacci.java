3fcd03b635c53635abd1e5638c9522e4fe296e8b
Nese <neseilhannn@gmail.com>
2024-04-29 21:57:08 +0300

    fibonacci as function

---
+++ src/Fibonacci/fibonacci.java
@@ -0,0 +1,19 @@
+package Fibonacci;
+import java.util.Scanner;
+
+public class fibonacci {
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+
+
+    public static void main(String[]Args){
+        Scanner inp = new Scanner(System.in);
+        System.out.println(fib(7));
+    }
+}
+
