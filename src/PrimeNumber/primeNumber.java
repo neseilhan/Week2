88cc407ab4f9bb0698605477d6f07f4ac3f4ab38
Nese <neseilhannn@gmail.com>
2024-05-01 21:11:02 +0300

    prime number fixed.

--- src/PrimeNumber/primeNumber.java
+++ src/PrimeNumber/primeNumber.java
@@ -7,7 +7,7 @@ public class primeNumber {
         System.out.print("Lutfen bir tamsayi giriniz : ");
         int num = inp.nextInt();
 
-        if (isPrime(num, 2)) {
+        if (isPrime(num, 2)) { //bolenin 2den baslamasını istedik.
             System.out.println(num + " asal sayidir.");
         } else {
             System.out.println(num + " asal sayi degildir.");
@@ -15,11 +15,11 @@ public class primeNumber {
     }
 
     private static boolean isPrime(int num, int bolen) {
-        if (num < 2) {
+        if (num < 2) {  //0 ve 1 asal degildir ve - sayi girildiginde de asal degildir yazar.
             return false;
-        } else if (num == 2) {
+        } else if (num == 2) { // 2 asaldir.
             return true;
-        } else if (num % bolen == 0) {
+        } else if (num % bolen == 0) { //herhangi bir bolene tam bolunuyorsa asal degildir.
             return false;
         } else {
             return isPrime(num, bolen + 1);
