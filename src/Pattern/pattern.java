996b51997cece311db15a1f5ec6ced94bd36d022
Nese <neseilhannn@gmail.com>
2024-05-01 21:28:42 +0300

    pattern completed.

--- src/Pattern/pattern.java
+++ src/Pattern/pattern.java
@@ -6,20 +6,20 @@ public class pattern {
 
     public static void pat(int n) {
 
-        if(n<=0){
+        if(n<=0){ //0a geldiginde dur
             System.out.print(n +" ");
             return;
         }
         System.out.print(n + " ");
          pat(n - 5);
-        System.out.print(n + " ");
+        System.out.print(n + " "); //tersten de gormek icin once ve sonrasına yazdırdık.
     }
 
     public static void main(String[]Args){
         Scanner inp = new Scanner(System.in);
         System.out.println("Desen icin bir n sayisi girin: ");
         int n = inp.nextInt();
-        pat(n);
+        pat(n); //yazdirma islemini method icinde yaptigimiz icin sadece methodu cagirdik.
 
 
     }
