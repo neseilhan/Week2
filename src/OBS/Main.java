c490468ada9b1a7b3ca4e636853b5997576d4841
Nese <neseilhannn@gmail.com>
2024-05-03 16:13:02 +0300

    OBS improved.

--- src/OBS/Main.java
+++ src/OBS/Main.java
@@ -5,6 +5,9 @@ public class Main {
         Course mat = new Course("Matematik", "MAT101", "MAT");
         Course fizik = new Course("Fizik", "FZK101", "FZK");
         Course kimya = new Course("Kimya", "KMY101", "KMY");
+        Course sozluMat = new Course("Matematik Sozlu", "MAT101", "MATSOZ");
+        Course sozluFiz = new Course("Fizik Sozlu", "FZK101", "FZKSOZ");
+        Course sozluKim = new Course("Kimya Sozlu", "KMY101", "KMYSOZ");
 
         Teacher t1 = new Teacher("Serdar Yil", "90550000000", "MAT");
         Teacher t2 = new Teacher("Ozlem Isik", "90550000001", "FZK");
@@ -14,15 +17,15 @@ public class Main {
         fizik.addTeacher(t2);
         kimya.addTeacher(t3);
 
-        Student s1 = new Student("Nese ilhan", 4, "140144015", mat, fizik, kimya);
-        s1.addBulkExamNote(50,20,40);
+        Student s1 = new Student("Nese ilhan", 4, "140144015", mat, fizik, kimya, sozluFiz, sozluKim, sozluMat);
+        s1.addBulkExamNote(50,20,40, 60,80,0);
         s1.isPass();
-        Student s2 = new Student("Esra Duran", 4, "2211133", mat, fizik, kimya);
-        s2.addBulkExamNote(100,50,40);
+        Student s2 = new Student("Esra Duran", 4, "2211133", mat, fizik, kimya, sozluFiz, sozluKim, sozluMat);
+        s2.addBulkExamNote(100,50,40,55,85,90);
         s2.isPass();
 
-        Student s3 = new Student("Sukran corduk", 4, "221121312", mat, fizik, kimya);
-        s3.addBulkExamNote(50,20,40);
+        Student s3 = new Student("Sukran corduk", 4, "221121312", mat, fizik, kimya,sozluFiz, sozluKim, sozluMat);
+        s3.addBulkExamNote(50,20,40,85,75,100);
         s3.isPass();
 
     }
--- src/OBS/Student.java
+++ src/OBS/Student.java
@@ -6,20 +6,26 @@ public class Student {
     Course mat;
     Course fizik;
     Course kimya;
+    Course sozluFiz;
+    Course sozluMat;
+    Course sozluKim;
     double average;
     boolean isPass;
 
-    Student(String name,int classes, String studentNo,Course mat, Course fizik, Course kimya){
+    Student(String name,int classes, String studentNo,Course mat, Course fizik, Course kimya, Course sozluFiz, Course sozluMat, Course sozluKim){
         this.name = name;
         this.classes = classes;
         this.studentNo = studentNo;
         this.mat = mat;
         this.fizik = fizik;
         this.kimya = kimya;
-        calcAvarage();
+        this.sozluFiz = sozluFiz;
+        this.sozluMat = sozluMat;
+        this.sozluKim = sozluKim;
+        calcAverage();
         this.isPass = false;
     }
-    public void addBulkExamNote(int mat, int fizik, int kimya){
+    public void addBulkExamNote(int mat, int fizik, int kimya, int sozluFiz, int sozluKim, int sozluMat ){
         if(mat >= 0 && mat <= 100){
             this.mat.note = mat;
         }
@@ -30,9 +36,20 @@ public class Student {
         if (kimya >= 0 && kimya <= 100) {
             this.kimya.note = kimya;
         }
+        if(sozluFiz >= 0 && sozluFiz <= 100){
+            this.sozluFiz.note = sozluFiz;
+        }
+        if(sozluMat >= 0 && sozluMat <= 100){
+            this.sozluMat.note = sozluMat;
+        }
+
+        if (sozluKim >= 0 && sozluKim <= 100) {
+            this.sozluKim.note = sozluKim;
+        }
     }
+
     public void isPass(){
-        if(this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0){
+        if(this.mat.note == 0 || this.fizik.note == 0 || this.kimya.note == 0 ||this.sozluMat.note == 0 || this.sozluFiz.note == 0 || this.sozluKim.note == 0){
             System.out.println("Notlar tam olarak girilmemis.");
         }
         else{
@@ -47,11 +64,11 @@ public class Student {
             }
         }
     }
-    public void calcAvarage(){
-        this.average = (this.fizik.note+ this.kimya.note + this.mat.note) / 3;
+    public void calcAverage(){
+        this.average = (((this.fizik.note*0.80)+(this.sozluFiz.note*0.20))+ ((this.kimya.note*0.80)+(this.sozluKim.note*0.20)) + ((this.mat.note*0.80)+(this.sozluMat.note*0.20))) / 3;
     }
     public boolean isCheckPass(){
-        calcAvarage();
+        calcAverage();
         return this.average > 55;
     }
     public  void printNote(){
@@ -60,5 +77,8 @@ public class Student {
         System.out.println("Matematik Notu : " + this.mat.note);
         System.out.println("Fizik Notu : " + this.fizik.note);
         System.out.println("Kimya Notu : " + this.kimya.note);
+        System.out.println("Matematik sozlu Notu : " + this.sozluMat.note);
+        System.out.println("Fizik sozlu Notu : " + this.sozluFiz.note);
+        System.out.println("Kimya sozlu Notu : " + this.sozluKim.note);
     }
 }
