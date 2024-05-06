46833559a31d4b83a582600d1fcad240ba33213e
Nese <neseilhannn@gmail.com>
2024-05-06 17:00:09 +0300

    repeatedNumbers completed.

---
+++ src/RepeatedNumbers/repeatedNumbers.java
@@ -0,0 +1,40 @@
+package RepeatedNumbers;
+
+import java.util.Arrays;
+
+public class repeatedNumbers {
+    static boolean isFind(int[] arr, int value){
+        for (int i : arr){
+            if(i == value){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+         int [] list = {3,7,3,3,2,34,10,21,1,34,10,1};
+         int[] duplicate = new int[list.length];
+         int startIndex = 0;
+
+         for(int i = 0; i<list.length; i++){
+               for(int j =0; j<list.length; j++) {
+                   if ((i != j) && (list[i] == list[j])) {      //ayni elemanla karsılasması durumu
+                           if (!isFind(duplicate, list[i])) { // i ve j birbirine esit olacagindan j de diyebilirdik.
+                              duplicate[startIndex++] = list[i]; //listin i. degerine at.
+                           }
+                       break;
+                   }
+               }
+         }
+        System.out.println(Arrays.toString(duplicate));
+
+        for(int value : duplicate){ //duplicate içindeki boş elemanlari yazdirmamak için bir dongu.
+            if(value != 0){
+                if(value%2 == 0) { // sadece cift degerleri yazdir.
+                    System.out.println(value);
+                }
+            }
+        }
+    }
+
+}
