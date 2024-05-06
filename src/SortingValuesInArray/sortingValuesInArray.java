557747ad25fded8e07683f03eb2247c053a1ae23
Nese <neseilhannn@gmail.com>
2024-05-06 18:50:33 +0300

    sortingValuesInArray completed.

--- src/SortingValuesInArray/sortingValuesInArray.java
+++ src/SortingValuesInArray/sortingValuesInArray.java
@@ -1,7 +1,23 @@
 package SortingValuesInArray;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class sortingValuesInArray {
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Lutfen olusturmak istediginiz dizinin boyutunu giriniz : ");
+        int lengthOfArray = inp.nextInt();
+        int[] list = new int[lengthOfArray];
+        System.out.println("Dizi boyutu : "+lengthOfArray);
+
+        for (int i = 0; i < lengthOfArray; i++) {  //tek tek elemanlari al ve "list" adli arrray'e at
+            System.out.print("Sayilar :  " + (i + 1) + ": ");
+            list[i] = inp.nextInt();
+        }
+
+        Arrays.sort(list); //kucukten buyuge sirala ve yazdir.
+        System.out.println("Sirali dizi : "+Arrays.toString(list));
 
     }
 }
